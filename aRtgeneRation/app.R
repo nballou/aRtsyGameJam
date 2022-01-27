772b96287c40f357d208807f275390d729c7b753
Nick <nick@NickMBP.local>
2022-01-27 14:49:26 +0000

    reactive shiny ui

--- aRtgeneRation/app.R
+++ aRtgeneRation/app.R
@@ -3,6 +3,7 @@ library(shinyWidgets)
 library(colourpicker)
 library(shinyjqui)
 library(aRtsy)
+library(randomcoloR)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -20,6 +21,7 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
+
             selectInput(inputId = "generator",
                         label = "Generator",
                         choices = c("Squares",
@@ -32,28 +34,16 @@ ui <- fluidPage(
                          value = 8675309
             ),
 
-            checkboxInput(inputId = "chooseOwnColors",
-                          label = "Choose colors manually?",
-                          value = FALSE),
-
-
-            conditionalPanel(
-                condition = "input.chooseOwnColors == 1",
-                colourInput(inputId = "color1",
-                            label = "Color 1",
-                            value = "#317da3"),
-
-                colourInput(inputId = "color2",
-                            label = "Color 2",
-                            value = "#cdd690"),
-
-                colourInput(inputId = "color3",
-                            label = "Color 3",
-                            value = "#a63f6a"),
+            radioGroupButtons(inputId = "colorChoice",
+                                 label = "How would you like to choose colors?",
+                                 choices = c("Color Palette" = "colorPalette",
+                                             "Manual colors" = "manualColors",
+                                             "Manual gradient" = "manualGradient")
             ),
 
             conditionalPanel(
-                condition = "input.chooseOwnColors == 0",
+                condition = "input.colorChoice == 'colorPalette'",
+
                 selectInput(inputId = "colorPalette",
                             label = "Color Palette",
                             choices = c("Dark Autumn" = "dark1",
@@ -61,25 +51,50 @@ ui <- fluidPage(
                 ),
             ),
 
+            conditionalPanel(
+                condition = "input.colorChoice == 'manualColors'",
 
-            # sliderInput(inputId = "plotHeight",
-            #              label = "Plot Height (Pixels)",
-            #              min = 100,
-            #              max = 3000,
-            #              value = 500),
-            #
-            # sliderInput(inputId = "plotWidth",
-            #              label = "Plot Width (Pixels)",
-            #              min = 100,
-            #              max = 3000,
-            #              value = 500),
+                sliderInput(inputId = "numColors",
+                            label = "Number of Colors",
+                            min = 1,
+                            max = 6,
+                            value = 2),
 
+                uiOutput("manualColors")
+
+                # colourInput(inputId = "color1",
+                #             label = "Color 1",
+                #             value = "#317da3"),
+                #
+                # colourInput(inputId = "color2",
+                #             label = "Color 2",
+                #             value = "#cdd690"),
+                #
+                # colourInput(inputId = "color3",
+                #             label = "Color 3",
+                #             value = "#a63f6a"),
+            ),
+
+
+            conditionalPanel(
+                condition = "input.colorChoice == 'manualGradient'",
+
+                colourInput(inputId = "color1",
+                            label = "Color 1",
+                            value = "#317da3"),
+
+                colourInput(inputId = "color2",
+                            label = "Color 2",
+                            value = "#cdd690"),
+            ),
+
+            # Options for "Squares" generator
             conditionalPanel(
                 condition = "input.generator == 'Squares'",
 
-                colourInput(inputId = "borderColor",
-                            label = "Border color",
-                            value = "Black"),
+                # colourInput(inputId = "borderColor",
+                #             label = "Border color",
+                #             value = "Black"),
 
                 sliderInput(inputId = "cuts",
                             label = "Cuts",
@@ -99,51 +114,67 @@ ui <- fluidPage(
                             min = 50,
                             max = 1000,
                             value = 200)
-                # selectInput(inputId = "colors",
-                #             label = "Color Palette",
-                #             choices = c("Dark Autumn" = "dark1",
-                #                         "Gustav Klimt" = "klimt"),
                 ),
         ),
 
-            # Show a plot of the generated distribution
+            # Create resizable image as output
             mainPanel(
                 jqui_resizable(plotOutput('aRt', width = '750px', height = '750px')),
             )
         )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic
 server <- function(input, output) {
 
-    output$aRt <- renderPlot(
-        # height = function() input$plotHeight,
-        # width = function() input$plotWidth,
-        {
+    # Dynamically create color selectors based on the number of colors chosen
+    output$manualColors <- renderUI({
+        lapply(1:input$numColors, function(i) {
+            colourInput(inputId = paste0('manualColor', i),
+                        label = paste0('Color ', i),
+                        value = randomColor())
+        })
+    })
+
+    output$aRt <- renderPlot({
+
             set.seed(input$seed)
 
             if (input$generator == "Squares") {
 
-                if (input$chooseOwnColors == TRUE) {
-                    plotColors = c(input$color1, input$color2, input$color3)
+                if (input$colorChoice == "manualColors") {
+                    plotColors <- NULL
+                    testColor = input$manualColor1
+                    inputValues = isolate(reactiveValuesToList(input))
+                    # print(inputValues)
+                    plotColors = inputValues[grepl("manualColor", names(inputValues), fixed = TRUE)]
+                    print(plotColors)
                 }
 
-                else if (input$chooseOwnColors == FALSE) {
-                    plotColors = colorPalette(input$colorPalette)
-                }
-                canvas_squares(colors = plotColors,
-                               background = input$borderColor,
-                               cuts = input$cuts,
-                               ratio = input$ratio,
-                               resolution = input$resolution)
+                art <- canvas_squares(colors = plotColors,
+                                      background = input$borderColor,
+                                      cuts = input$cuts,
+                                      ratio = input$ratio,
+                                      resolution = input$resolution)
+
+                # else if (input$chooseOwnColors == FALSE) {
+                #     plotColors = colorPalette(input$colorPalette)
+                # }
+
+                #
+                # if (input$chooseOwnColors == TRUE) {
+                #     art <- art +
+                #         ggplot2::scale_fill_gradient(low = input$color1, high = input$color2)
+                # }
             }
+
+
 
             else if (input$generator == "Ribbons") {
-                canvas_ribbons(colors = colorPalette(input$colors))
+                art <- canvas_ribbons(colors = colorPalette(input$colors))
             }
-
-        }
-    )
+            art
+    })
 }
 
 # Run the application
